f65f0e170644a3aa24105a22ab0f7bad1318561e
Rahim Jamal <rahim_92@live.ca>
2022-10-28 15:08:37 -0400

    final push

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,27 @@
 const request = require('request');
-let breedName = process.argv[2];
-const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
-request(url, (error, response, body) => {
-  if (error) {
-    console.log('error:', error); // Print the error if one occurred
-  }
-  if (response) {
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  }
-  const data = JSON.parse(body);
-  const breed = data[0];
-  if (breed) {
-    console.log(breed.description);
-  } else {
-    console.log('Failed to find breed:' + process.argv[2]);
-  }
-});
+
+
+
+const fetchBreedDescription = function(breedName, callback) {
+  const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    }
+    const data = JSON.parse(body);
+    const breed = data[0];
+    if (breed) {
+      callback(null, breed.description);
+    } else {
+      callback('Failed to find breed:', null);
+    }
+
+  });
+};
+
+
+
+
+
+module.exports = { fetchBreedDescription };
 
